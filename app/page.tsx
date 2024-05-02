9f5af4a97a58e1d5c7eed4161c12eea9ade602d6
Yasmim <barrosyasmim13@gmail.com>
2024-05-02 00:40:43 -0300

    feat: add promo banner to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,31 @@
 import Header from "./_components/header";
 import Search from "./_components/search";
 import CategoryList from "./_components/categoy-list";
+import Image from "next/image";
 
 const Home = () => {
   return (
     <>
       <Header />
+
       <div className="px-5 pt-6">
         <Search />
       </div>
+
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-6 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );

69414ad7492d3a356c507b7989e8b6554d04c87a
Yasmim <barrosyasmim13@gmail.com>
2024-05-02 04:35:06 -0300

    feat: add restaurant list component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "./_components/header";
 import Search from "./_components/search";
 import CategoryList from "./_components/categoy-list";
-import Image from "next/image";
 import ProductList from "./_components/Product-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
+import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -37,14 +38,9 @@ const Home = async () => {
       </div>
 
       <div className="px-6 pt-6">
-        <Image
+        <PromoBanner
           src="/promo-banner-01.png"
           alt="Até 30% de desconto em pizzas"
-          height={0}
-          width={0}
-          className="h-auto w-full object-contain"
-          sizes="100vw"
-          quality={100}
         />
       </div>
 
@@ -60,6 +56,27 @@ const Home = async () => {
           </Button>
         </div>
         <ProductList products={products} />
+      </div>
+
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/banner-burguer.png"
+          alt="A partir de R$17,90 em lanches"
+        />
+      </div>
+
+      <div className="space-y-4 py-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </>
   );

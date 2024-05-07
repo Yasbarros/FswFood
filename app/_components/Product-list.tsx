926e25fedc9e369e1674cdae2a29835e668dcc00
Yasmim <barrosyasmim13@gmail.com>
2024-05-07 00:55:19 -0300

    feat: product page and juice list

--- app/_components/Product-list.tsx
+++ app/_components/Product-list.tsx
@@ -13,7 +13,7 @@ interface ProductListProps {
   }>;
 }
 
-const ProducList = async ({ products }: ProductListProps) => {
+const ProducList = ({ products }: ProductListProps) => {
   return (
     <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
